f4f85fa377e215338623141a49e1a4eade7dc442
Eduardo Costa <m4c0@users.noreply.github.com>
2022-03-09 18:22:09 +0000

    ef: refactor in depmgmt>dep

--- lib/efpom.ml
+++ lib/efpom.ml
@@ -24,19 +24,6 @@ let properties_of t = Properties.to_seq t.properties
 let depmgmt_of t = Dependency.Map.to_seq t.depmgmt |> Seq.map (fun (_, v) -> v)
 let deps_of t = Dependency.Map.to_seq t.deps |> Seq.map (fun (_, v) -> v)
 
-let id_of_parsed (p : Parser.t) parent =
-  match parent with
-  | Some (pg, _, pv) ->
-      let g = Option.value p.id.group ~default:pg in
-      let v = Option.value p.id.version ~default:pv in
-      (g, p.id.artifact, v)
-  | None ->
-      if p.id.group = None then failwith "missing groupId";
-      if p.id.version = None then failwith "missing version";
-      let g = Option.get p.id.group in
-      let v = Option.get p.id.version in
-      (g, p.id.artifact, v)
-
 let dep_of_parsed dmfn (d : Dependency.t) : dep =
   {
     classifier = Dependency.classifier_of d;
@@ -48,20 +35,11 @@ let dep_of_parsed dmfn (d : Dependency.t) : dep =
     is_bom = Dependency.is_bom d;
   }
 
-let depmap_from_seq dm ps parent =
-  let pdm = match parent with None -> Dependency.Map.empty | Some pp -> pp in
-  let mfn _ a b = match b with None -> a | _ -> b in
+let depmap_from_seq dm ps =
   ps
   |> Seq.map Dependency.unique_key
   |> Dependency.Map.of_seq
   |> Dependency.Map.map (dep_of_parsed dm)
-  |> Dependency.Map.merge mfn pdm
-
-let merged_props props (parent : t option) =
-  let p0 = Properties.of_seq props in
-  match parent with
-  | None -> p0
-  | Some pp -> Properties.merge_left p0 pp.properties
 
 let resolve_id (props : Properties.t) ((g, a, v) : id) =
   let apply = Properties.apply props in
@@ -76,37 +54,18 @@ let resolve_depmap (props : Properties.t) (dm : dep) =
   in
   { dm with id; classifier }
 
-let rec inheritor fname : t =
-  let p = Parser.parse_file fname in
-  let parent = p.parent in
-  let parent_p =
-    Option.map (Repo.parent_of_pom fname) p.parent |> Option.map inheritor
-  in
-  let id = id_of_parsed p parent in
-  let properties = merged_props p.props parent_p in
-
-  let depmgmt =
-    Option.map (fun p -> p.depmgmt) parent_p
-    |> depmap_from_seq (fun _ -> None) p.dep_mgmt
-  in
-  let dmfn k =
-    Dependency.Map.find_opt k depmgmt
-    |> Option.map (fun ({ id = _, _, v; _ } : dep) -> v)
-  in
-  let deps =
-    Option.map (fun p -> p.deps) parent_p |> depmap_from_seq dmfn p.deps
-  in
-  { id; parent; properties; depmgmt; deps }
-
 let rec from_pom fname : t =
-  let i = inheritor fname in
+  let i = Inheritor.parse fname in
+
   let properties =
     Properties.of_id i.id
     |> Properties.merge_right i.properties
     |> Properties.resolve
   in
+
   let all_dm =
-    Dependency.Map.map (resolve_depmap properties) i.depmgmt
+    depmap_from_seq (fun _ -> None) i.depmgmt
+    |> Dependency.Map.map (resolve_depmap properties)
     |> Dependency.Map.to_seq
   in
   let read_bom (_, ({ id = g, a, v; _ } : dep)) =
@@ -122,6 +81,13 @@ let rec from_pom fname : t =
     |> Seq.flat_map read_bom
   in
   let depmgmt = Seq.append bom non_bom |> Dependency.Map.of_seq in
-  { i with properties; depmgmt }
+
+  let dmfn k =
+    Dependency.Map.find_opt k depmgmt
+    |> Option.map (fun ({ id = _, _, v; _ } : dep) -> v)
+  in
+  let deps = depmap_from_seq dmfn i.deps in
+
+  { id = i.id; parent = i.parent; properties; depmgmt; deps }
 
 let from_java fname = Repo.pom_of_java fname |> from_pom
---
+++ lib/inheritor.ml
@@ -0,0 +1,46 @@
+type id = string * string * string
+
+type t = {
+  id : id;
+  parent : id option;
+  properties : Properties.t;
+  depmgmt : Dependency.t Seq.t;
+  deps : Dependency.t Seq.t;
+}
+
+let id_of_parsed (p : Parser.t) parent =
+  match parent with
+  | Some (pg, _, pv) ->
+      let g = Option.value p.id.group ~default:pg in
+      let v = Option.value p.id.version ~default:pv in
+      (g, p.id.artifact, v)
+  | None ->
+      if p.id.group = None then failwith "missing groupId";
+      if p.id.version = None then failwith "missing version";
+      let g = Option.get p.id.group in
+      let v = Option.get p.id.version in
+      (g, p.id.artifact, v)
+
+let merged_props props (parent : t option) =
+  let p0 = Properties.of_seq props in
+  match parent with
+  | None -> p0
+  | Some pp -> Properties.merge_left p0 pp.properties
+
+let rec parse fname : t =
+  let p = Parser.parse_file fname in
+  let parent = p.parent in
+  let parent_p =
+    Option.map (Repo.parent_of_pom fname) p.parent |> Option.map parse
+  in
+  let id = id_of_parsed p parent in
+  let properties = merged_props p.props parent_p in
+
+  let parent_dm = Option.to_seq parent_p |> Seq.flat_map (fun p -> p.depmgmt)  in
+  let depmgmt = Seq.append parent_dm p.dep_mgmt  in
+  
+  let parent_deps = Option.to_seq parent_p |> Seq.flat_map (fun p -> p.deps)  in
+  let deps = Seq.append parent_deps p.deps  in
+
+  { id; parent; properties; depmgmt; deps }
+
